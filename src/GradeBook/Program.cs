27647293abfc5f86e35bfc2bcfdef75af117abb5
srmedan <44142802+srmedan@users.noreply.github.com>
2020-02-02 15:03:52 +0200

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] numbers = new double[3];
+            numbers[0] = 4;
+            numbers[1] = 9;
+            numbers[2] = 12;
+
+            double result = numbers[0] + numbers[1] + numbers[2];
+                        
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
