e89009a85411b72ad0cca50beb1a87e8b7aa033e
srmedan <44142802+srmedan@users.noreply.github.com>
2020-02-08 22:50:43 +0200

    various changes + unit testing files added

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 
 {
-    class Book
+   public class Book
     {
         public Book(string name)
         {
@@ -14,6 +15,26 @@ namespace GradeBook
         {
             
             grades.Add(grade);
+
+        }
+        public Statistics GetStatistics()
+
+        {
+
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach(double grade in grades) 
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+            
+            return result;
 
         }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,23 +12,13 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(90.5);
+            
+            var stats = book.GetStatistics();
            
-            var grades = new List<double>() {12.5, 10.4, 15.6};
-            grades.Add(56.1);
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(double number in grades) 
-            {
-               lowGrade = Math.Min(number, lowGrade);
-               highGrade = Math.Max(number, highGrade);
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The highest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+           
         }
     }
 
