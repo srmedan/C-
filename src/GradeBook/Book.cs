e011d40afd95aa87d576b7c2f239bc2fdf2927dd
srmedan <44142802+srmedan@users.noreply.github.com>
2020-02-18 21:57:03 +0200

    FOR loop

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -37,13 +37,11 @@ namespace GradeBook
             result.Low = double.MaxValue;
 
 
-            var index = 0;
-            while(index < grades.Count)
+            for(var index = 0; index < grades.Count; index += 1)
             {
                 result.Low = Math.Min(grades[index], result.Low);
                 result.High = Math.Max(grades[index], result.High);
                 result.Average += grades[index];
-                index += 1;
             } 
             result.Average /= grades.Count;
             
