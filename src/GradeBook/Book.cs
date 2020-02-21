8035cbe574844541456a52153dbbb7dd40300793
srmedan <44142802+srmedan@users.noreply.github.com>
2020-02-21 23:54:57 +0200

    pattern matching with Switch

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -15,13 +15,22 @@ namespace GradeBook
 
         public void AddLetterGrade(char letter)
         {
-            if(letter == 'A')
+            switch(letter)
             {
-                AddGrade(90);
-            }
-            else if(letter == 'B')
-            {
-                
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
             }
         }
 
@@ -64,6 +73,29 @@ namespace GradeBook
             } 
             result.Average /= grades.Count;
             
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
 
         }
---
+++ src/GradeBook/test.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+    public class Test
+    {
+        public static void Print()
+        {
+            Console.WriteLine("This is a test");
+        }
+    }
+
+
+}
